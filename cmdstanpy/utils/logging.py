c728ab0ca1094951d1051fb66c8857ff63e9745f
amas <andrew.mascioli1@gmail.com>
2025-06-17 21:40:12 -0400

    Restructure enable/disable logging
    Now have EnableLogging and DisableLogging classes
    that are returned by enable_logging and disable_logging
    functions. This helps avoid breaking too many conventions
    regarding class naming schemes

--- cmdstanpy/utils/logging.py
+++ cmdstanpy/utils/logging.py
@@ -1,15 +1,18 @@
 """
 CmdStanPy logging
 """
+
 import functools
 import logging
+import types
 from contextlib import AbstractContextManager
+from typing import Optional, Type
 
 
 @functools.lru_cache(maxsize=None)
 def get_logger() -> logging.Logger:
     """cmdstanpy logger"""
-    logger = logging.getLogger('cmdstanpy')
+    logger = logging.getLogger("cmdstanpy")
     if not logger.hasHandlers():
         # send all messages to handlers
         logger.setLevel(logging.DEBUG)
@@ -18,7 +21,7 @@ def get_logger() -> logging.Logger:
         handler.setLevel(logging.INFO)
         handler.setFormatter(
             logging.Formatter(
-                '%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+                "%(asctime)s - %(name)s - %(levelname)s - %(message)s",
                 "%H:%M:%S",
             )
         )
@@ -26,31 +29,47 @@ def get_logger() -> logging.Logger:
     return logger
 
 
-class enable_logging(AbstractContextManager):
-    """Enable cmdstanpy logging. Can be used as a context manager"""
-
+class EnableLogging(AbstractContextManager):
     def __init__(self) -> None:
         self.logger = get_logger()
         self.prev_state = self.logger.disabled
         self.logger.disabled = False
 
-    def __enter__(self):
+    def __enter__(self) -> "EnableLogging":
         return self
 
-    def __exit__(self, exc_type, exc_value, traceback) -> None:
+    def __exit__(
+        self,
+        exc_type: Optional[Type[BaseException]],
+        exc_value: Optional[BaseException],
+        traceback: Optional[types.TracebackType],
+    ) -> None:
         self.logger.disabled = self.prev_state
 
 
-class disable_logging(AbstractContextManager):
-    """Disable cmdstanpy logging. Can be used as a context manager"""
-
+class DisableLogging(AbstractContextManager):
     def __init__(self) -> None:
         self.logger = get_logger()
         self.prev_state = self.logger.disabled
         self.logger.disabled = True
 
-    def __enter__(self):
+    def __enter__(self) -> "DisableLogging":
         return self
 
-    def __exit__(self, exc_type, exc_value, traceback) -> None:
+    def __exit__(
+        self,
+        exc_type: Optional[Type[BaseException]],
+        exc_value: Optional[BaseException],
+        traceback: Optional[types.TracebackType],
+    ) -> None:
         self.logger.disabled = self.prev_state
+
+
+def enable_logging() -> EnableLogging:
+    """Enable cmdstanpy logging. Can be used as a context manager"""
+    return EnableLogging()
+
+
+def disable_logging() -> DisableLogging:
+    """Disable cmdstanpy logging. Can be used as a context manager"""
+    return DisableLogging()
