bba3bdeec3cbbfc1ea20d18e22d093aa241923f6
amas <andrew.mascioli1@gmail.com>
2025-07-26 16:35:30 -0400

    Add numpy/polars equiv testing

--- test/test_stancsv.py
+++ test/test_stancsv.py
@@ -260,3 +260,48 @@ def test_parsing_adaptation_lines_no_free_params():
     ]
     _, mass_matrix = stancsv.parse_hmc_adaptation_lines(lines)
     assert mass_matrix is None
+
+
+def test_csv_polars_and_numpy_equiv():
+    lines = [
+        b"-6.76206,1,0.787025,1,1,0,6.81411,0.229458\n",
+        b"-6.81411,0.983499,0.787025,1,1,0,6.8147,0.20649\n",
+        b"-6.85511,0.994945,0.787025,2,3,0,6.85536,0.310589\n",
+        b"-6.85511,0.812189,0.787025,1,1,0,7.16517,0.310589\n",
+    ]
+    arr_out_polars = stancsv.csv_bytes_list_to_numpy(
+        lines, includes_header=False
+    )
+    with mock.patch.dict("sys.modules", {"polars": None}):
+        arr_out_numpy = stancsv.csv_bytes_list_to_numpy(
+            lines, includes_header=False
+        )
+    assert np.array_equiv(arr_out_polars, arr_out_numpy)
+
+
+def test_csv_polars_and_numpy_equiv_one_line():
+    lines = [
+        b"-6.76206,1,0.787025,1,1,0,6.81411,0.229458\n",
+    ]
+    arr_out_polars = stancsv.csv_bytes_list_to_numpy(
+        lines, includes_header=False
+    )
+    with mock.patch.dict("sys.modules", {"polars": None}):
+        arr_out_numpy = stancsv.csv_bytes_list_to_numpy(
+            lines, includes_header=False
+        )
+    assert np.array_equiv(arr_out_polars, arr_out_numpy)
+
+
+def test_csv_polars_and_numpy_equiv_one_element():
+    lines = [
+        b"-6.76206\n",
+    ]
+    arr_out_polars = stancsv.csv_bytes_list_to_numpy(
+        lines, includes_header=False
+    )
+    with mock.patch.dict("sys.modules", {"polars": None}):
+        arr_out_numpy = stancsv.csv_bytes_list_to_numpy(
+            lines, includes_header=False
+        )
+    assert np.array_equiv(arr_out_polars, arr_out_numpy)
