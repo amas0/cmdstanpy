cdb9d9d72fd0e84351be300598f7eb64d36f20b8
amas <andrew.mascioli1@gmail.com>
2025-11-18 08:57:02 -0500

    Add save_metric=1 to adapt sampler args

--- cmdstanpy/cmdstan_args.py
+++ cmdstanpy/cmdstan_args.py
@@ -273,6 +273,9 @@ class SamplerArgs:
             cmd.append(f'window={self.adapt_metric_window}')
         if self.adapt_step_size is not None:
             cmd.append('term_buffer={}'.format(self.adapt_step_size))
+        cmd.append('save_metric=1')
+        # End adapt subsection
+
         if self.num_chains > 1:
             cmd.append('num_chains={}'.format(self.num_chains))
 
